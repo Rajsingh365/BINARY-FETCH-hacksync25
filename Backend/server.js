7c2657bb83f2fe4ef0c4afa64274e6f759d48d51
Taha <tahaalotwala@gmail.com>
2025-02-23 23:59:37 +0530

    node backend : create routes for listener functionality

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ import fileUpload from "express-fileupload";
 
 import authRoutes from "./routes/auth.route.js";
 import podcastRouter from "./routes/podcast.routes.js";
-import appRouter from "./routes/appRoutes.js";
+import appRouter from "./routes/app.routes.js";
+import listenerRouter from "./routes/listener.routes.js";
 import contentGenerationRoutes from "./routes/contentgeneration.route.js";
 import { protectRoute } from "./middleware/authentication.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -36,7 +37,8 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/content-generation", contentGenerationRoutes);
 app.use("/api/podcast", protectRoute, podcastRouter);
-app.use("/api/app", appRouter);
+app.use("/api/app", protectRoute, appRouter);
+app.use("/api/listener", listenerRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
